c84649f0c3a31366efa98a66080fc6c8ea31d15a
DevDaviSVC <davisouzavieiradacosta@gmail.com>
2025-05-12 16:20:25 -0300

    Finishing models making of page.

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 const nav = document.querySelector("nav");
 const menu = document.getElementById('menu');
-const processorSvg = document.getElementById("processor-svg");
-const chartSvg = document.getElementById("chart-svg");
-const codeSvg = document.getElementById("code-svg");
+const ecoSvg = document.getElementById("eco-svg");
+const eco2Svg = document.getElementById("eco2-svg");
+const recycleSvg = document.getElementById("recycle-svg");
 const heroContent = document.getElementsByClassName("hero-content-text");
 
 window.onload = () => {
@@ -18,9 +18,9 @@ window.addEventListener("scroll", function() {
 
         nav.classList.toggle("sticky", window.scrollY > 0);
 
-        processorSvg.style.top = `${window.innerHeight * 0.3 + value * 0.4}px`;
-        chartSvg.style.top = `${window.innerHeight * 0.5 + value * 0.5}px`;
-        codeSvg.style.top = `${window.innerHeight * 0.6 + value * 0.7}px`;
+        ecoSvg.style.top = `${window.innerHeight * 0.2 + value * 0.4}px`;
+        eco2Svg.style.top = `${window.innerHeight * 0.4 + value * 0.5}px`;
+        recycleSvg.style.top = `${window.innerHeight * 0.5 + value * 0.7}px`;
         for (let i = 0; i < 2; i++) {
           heroContent[i].style.top = `${value * 0.3}px`;
         }
