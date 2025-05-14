eee1f788c4388df4499f11b8238db9cbf5d42604
DevDaviSVC <davisouzavieiradacosta@gmail.com>
2025-05-13 21:06:34 -0300

    Finishing project.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 const nav = document.querySelector("nav");
-const menu = document.getElementById('menu');
+const menu = document.getElementById("menu");
 const ecoSvg = document.getElementById("eco-svg");
 const eco2Svg = document.getElementById("eco2-svg");
 const recycleSvg = document.getElementById("recycle-svg");
@@ -8,31 +8,20 @@ const mmCloseBtn = document.getElementById("close-mobile-menu-btn");
 const mmBtn = document.getElementById("menu-btn");
 
 mmCloseBtn.onclick = () => {
-    document.getElementById("mobile-menu").classList.toggle("active", false);
+  document.getElementById("mobile-menu").classList.toggle("active", false);
 };
 
 mmBtn.onclick = () => {
   document.getElementById("mobile-menu").classList.toggle("active", true);
 };
 
-window.onload = () => {
-    if (window.innerWidth > 1000) {
-        nav.classList.toggle("sticky", window.scrollY > 0);
-    }
-};
+window.addEventListener("scroll", function () {
+  let value = window.scrollY;
 
-window.addEventListener("scroll", function() {
-    if (window.innerWidth > 1000) {
-        let value = window.scrollY;
-
-        nav.classList.toggle("sticky", window.scrollY > 0);
-
-        ecoSvg.style.top = `${window.innerHeight * 0.2 + value * 0.4}px`;
-        eco2Svg.style.top = `${window.innerHeight * 0.4 + value * 0.5}px`;
-        recycleSvg.style.top = `${window.innerHeight * 0.5 + value * 0.7}px`;
-        for (let i = 0; i < 2; i++) {
-          heroContent[i].style.top = `${value * 0.3}px`;
-        }
-    
-    }
+  ecoSvg.style.top = `${window.innerHeight * 0.2 + value * 0.4}px`;
+  eco2Svg.style.top = `${window.innerHeight * 0.4 + value * 0.5}px`;
+  recycleSvg.style.top = `${window.innerHeight * 0.5 + value * 0.7}px`;
+  for (let i = 0; i < 2; i++) {
+    heroContent[i].style.top = `${value * 0.3}px`;
+  }
 });
